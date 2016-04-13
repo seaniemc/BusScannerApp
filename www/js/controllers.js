b24cfc938ab1dcec2cad0fa4c7c8ded63c3ec3d3
Sean McGrath <Sean McGrath>
2016-04-13 19:35:43 +0100

    Added form validation with sass and ng-message Also added forms-errors.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ var app = angular.module('busscanner.controllers', []);
 
  app.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+app.controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -25,9 +25,9 @@ app.controller('LoginCtrl', function($scope, $state) {
   };
   
   $scope.login = function (form) {
-      
+       console.log("LoginCtrl::login");
       if(form.valid){
-          console.log("LoginCtrl::login");
+          
       }else{
           console.log("invalid form");
       }
