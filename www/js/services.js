84ee4a98d7afb7ea736687a5e15fcd911514e548
Sean McGrath <Sean McGrath>
2016-04-19 23:54:51 +0100

    Added some comments and tidied up my code

--- www/js/services.js
+++ www/js/services.js
@@ -86,33 +86,3 @@ app.service("UserService", ["$rootScope", '$timeout', '$q', function($rootScope,
 
 }]);
 
-app.service("PuchDBListener", ["$rootScope", function($rootScope) {
-
-  localDB.changes({
-    //continuous looking for changes
-    continuous: true,
-    //when change it calls the call back
-    onChange: function(change) {
-      //if the change is not a delete, then we tell rootscope there is a change coming 
-      if (!change.deleted) {
-        $rootScope.$apply(function() {
-          //get the change based on the id
-          localDB.get(change.id, function(err, doc) {
-            $rootScope.$apply(function() {
-              if (err) {
-                console.log(err);
-              }
-              //if its not an error broadcast the change
-              $rootScope.$broadcast('add', doc);
-            })
-          });
-        })
-      } else {
-        $rootScope.$apply(function() {
-          $rootScope.$broadcast('delete', change.id);
-        });
-      }
-    }
-  })
-  return true;
-}]);
